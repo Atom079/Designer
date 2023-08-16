ba289bbaa1644922b728294d53fc24e2133c65ab
Nursaidbek Qodirov <qnursaidbek@gmail.com>
2023-08-15 18:52:55 -0800

    i have finished explore work section in homepage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,6 @@
 window.addEventListener('DOMContentLoaded', () =>{
     const navbar = document.querySelector('.nav')
     window.addEventListener('scroll', ()=>{
-        if (window.scrollY >= 100) {
-            navbar.classList.add('active');
-        }else{
-            navbar.classList.remove('active')
-        }
+        navbar.classList.toggle('active', window.scrollY > 600)
     })
 })
