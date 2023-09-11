fb96820996ea67506dfb0bd141a651b9284ca3e0
Nursaidbek Qodirov <qnursaidbek@gmail.com>
2023-09-11 05:41:46 -0800

    Alhamdilulloh i have finished trending section

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,6 @@
 window.addEventListener('DOMContentLoaded', () =>{
     const navbar = document.querySelector('.nav')
     window.addEventListener('scroll', ()=>{
-        navbar.classList.toggle('active', window.scrollY > 600)
+        navbar.classList.toggle('active', window.scrollY > 100)
     })
 })
