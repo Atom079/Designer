f6676c48bbed68467170569fe700fb7564fb5bce
Nursaidbek Qodirov <qnursaidbek@gmail.com>
2023-09-13 08:05:24 -0800

    i have finished my work thank you everybody.!!!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,19 @@
-'use strict';
-window.addEventListener('DOMContentLoaded', () =>{
-    const navbar = document.querySelector('.nav')
-    window.addEventListener('scroll', ()=>{
-        navbar.classList.toggle('active', window.scrollY > 100)
-    })
-})
+"use strict";
+window.addEventListener("DOMContentLoaded", () => {
+  const navbar = document.querySelector(".nav");
+  window.addEventListener("scroll", () => {
+    navbar.classList.toggle("active", window.scrollY > 100);
+  });
+  // Responsive navbar js
+  const burger = document.querySelector(".burger");
+  const navList = document.querySelector(".nav-list");
+  const navListLink = document.querySelectorAll(".nav-list-link");
+  burger.addEventListener("click", () => {
+    navList.classList.toggle("active");
+  });
+  navListLink.forEach((item) => {
+    item.addEventListener("click", () => {
+      navList.classList.toggle("active");
+    });
+  });
+});
